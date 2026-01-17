78eedb84c1b680297edfe8ad0d5b8c18ae25cc40
MaximumReality <lorith79@gmail.com>
2026-01-17 20:29:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,23 @@
-const CACHE_NAME = 'mochkil-puffs-v1';
-const ASSETS = [
+const CACHE_NAME = 'mochkil-puffs-v2';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './game.js',
   './cereal_wars.mp3',
   './aisle.jpg',
-  './thief_no_cereal.png',
-  './mochkil_puffs.PNG',
-  './beanie.png',
-  './chef-hat.png',
-  './flyazulo.png',
-  './holding_azulos.png',
-  './azulos_special.png',
-  './azulos_corrupted.png',
-  './puffs_cereal.png',
-  './credibility_badge.png',
-  './puffs_commercial.MP4',
-  './8bit_azulo.MP4'
+  './azulos_corrupted.png'
 ];
 
-// Install the service worker and cache assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// Serve assets from cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
